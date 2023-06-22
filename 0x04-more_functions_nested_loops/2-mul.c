90eb08f53d886ec60aad4f8fec1943f783fbdd60
Dennis Ahadi Nzavi Kimeu <dennisnzavi@gamil.com>
2023-06-22 14:53:00 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - does product of two numbers
+ *@a: first number
+ *@b: second number
+ *Return: Displays the product of the two numbers
+ */
+
+int mul(int a, int b)
+
+{
+
+	return (a * b);
+
+}
