7abcc2791abb02404afe1bf825e59ec062ce14f8
Dennis Ahadi Nzavi Kimeu <dennisnzavi@gamil.com>
2023-07-01 04:59:25 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - "prints with proper grammar, but the outcome is a piece of art,
- * followed by anew line\n"
- * Return Always 0.
+ * main - Entry point 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,)\n");
