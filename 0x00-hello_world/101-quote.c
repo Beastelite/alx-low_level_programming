88a4710e65c06804607ec0cbebc395e465bc2695
Dennis Ahadi Nzavi Kimeu <dennisnzavi@gamil.com>
2023-07-01 05:16:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints
+ * main - Beginning part
+ *
+ * Return: 1 (Pass)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
