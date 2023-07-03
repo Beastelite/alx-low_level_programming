51b895e643d2acda47d2b982779f52c778c1f982
Dennis Ahadi Nzavi Kimeu <dennisnzavi@gamil.com>
2023-07-03 00:30:01 -0700

    0-reset_to_98 code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  *and updates the value it points to to 98
- * @n - value of the number
+ * @n: An integer input pointer
  * Return: nothing
  */
 void reset_to_98(int *n)
