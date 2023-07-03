b5ce0ad12ca5b399c0a68dfe0d1f84caee2ce1de
Dennis Nzavi <dennisnzavi@gmail.com>
2023-07-03 16:14:02 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ * Return: 0 on success
+ */
+int main (void)
+{
+	int c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
