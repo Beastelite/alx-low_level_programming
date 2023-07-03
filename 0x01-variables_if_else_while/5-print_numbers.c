0b90649810500a66a44a3f170702c9e02b739c35
Dennis Nzavi <dennisnzavi@gmail.com>
2023-07-03 16:32:32 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number from 0 to 9
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i)
+	printf("\n");
+	return (0);
+}
