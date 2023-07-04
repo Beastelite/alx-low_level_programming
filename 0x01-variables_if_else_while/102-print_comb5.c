770ce5f731e1efb026117190c18571a253ec52c6
Dennis Nzavi <dennisnzavi@gmail.com>
2023-07-04 03:17:14 -0700

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (i <= 99)
 	{
@@ -15,11 +15,11 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) +48);
-				putchar((i % 10) +48);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) +48);
-				putchar((j % 10) +48);
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 98)
 				{
 					putchar(',');
